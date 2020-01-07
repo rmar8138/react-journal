d3a46a4b2cca55c9350497d81ed218c37dfd1d6c
rmar8138 <ragan.martinez@live.com>
2020-01-08 10:44:28 +1100

    Create views for journal

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import HomePage from "./pages/HomePage";
+import CategorySelectionPage from "./pages/CategorySelectionPage";
+import NewEntryPage from "./pages/NewEntryPage";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>React Journal</h1>
+        <HomePage />
+        <CategorySelectionPage />
+        <NewEntryPage />
       </div>
     );
   }
---
+++ src/components/pages/NewEntryPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class NewEntryPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>New Entry Page</h1>
+      </div>
+    );
+  }
+}
