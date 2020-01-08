299d188904f59bb0a05e47b2d39943256e2852dd
rmar8138 <ragan.martinez@live.com>
2020-01-08 12:11:56 +1100

    Setup react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CategorySelectionPage from "./pages/CategorySelectionPage";
 import NewEntryPage from "./pages/NewEntryPage";
@@ -7,10 +8,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>React Journal</h1>
-        <HomePage />
-        <CategorySelectionPage />
-        <NewEntryPage />
+        <BrowserRouter>
+          <>
+            <Link to="/">Home</Link>
+            <Link to="/category">Category Selection</Link>
+            <Link to="/entry">New Entry</Link>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/category" component={CategorySelectionPage} />
+            <Route exact path="/entry" component={NewEntryPage} />
+          </>
+        </BrowserRouter>
       </div>
     );
   }
